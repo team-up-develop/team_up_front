14c421fb5030312317ae233068a0a89e510807b1
坂本一弥 <sskazuya1130@gmail.com>
2020-08-03 23:11:06 +0900

    storeの移行

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    idToken: null,
+    LoginName: "",
+    LoginPassword: "",
+    userId: null
   },
   mutations: {
+    updateIdToken(state, idToken) {
+      state.idToken = idToken;
+    },
+    // * localStorageにUserNameを保存し、判定する
+    createLocalStorage(state, userId) {
+      state.userId = userId
+      localStorage.userId = state.userId
+      // console.log(localStorage.loginName)
+    }
+    // createLocalStorage(state) {
+    //   localStorage.LoginName = authData.LoginName;
+    //   localStorage.LoginPassword = authData.LoginPassword;
+    //   console.log(localStorage)
+    // }
   },
   actions: {
-  },
-  modules: {
+    login({ commit }, authData) {
+      const data = {
+        LoginName: authData.LoginName,
+        LoginPassword: authData.LoginPassword
+      }
+      axios.post('http://localhost:8888/api/v1/login', data)
+      .then(response => {
+        console.log(response.data)
+        console.log(response.data.userId)
+        commit('updateIdToken', response.data.idToken)
+        commit('createLocalStorage', response.data.userId)
+      });
+    },
+    clearLocalStorage() {
+      // console.log("clearLocalStorage")
+    }
   }
 })
