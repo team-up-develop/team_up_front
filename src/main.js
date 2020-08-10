e0ad445d9d3ceb639ccea7dd238eac163ddad6d9
坂本一弥 <sskazuya1130@gmail.com>
2020-08-10 15:36:39 +0900

    案件上部に検索を導入 / vue内でjQueryを使用できるようになる

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'normalize.css'
+import jQuery from 'jquery'
+
+
+global.jquery = jQuery
+global.$ = jQuery
+window.$ = window.jQuery = require('jquery')
 
 Vue.config.productionTip = false
 
