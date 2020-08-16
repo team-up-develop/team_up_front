efff0035bcbdbf506aac784d2c461857e7e859ab
坂本一弥 <sskazuya1130@gmail.com>
2020-08-16 20:20:40 +0900

    管理画面 詳細 仮作成

--- src/main.js
+++ src/main.js
@@ -13,10 +13,10 @@ window.$ = window.jQuery = require('jquery')
 Vue.config.productionTip = false
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faLaptop, faUser, faBell, faHistory, faBars, faUserPlus, faBookmark, faComments, faArchive, faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import { faLaptop, faUser, faBell, faHistory, faBars, faUserPlus, faBookmark, faComments, faArchive, faAddressCard, faUsers, faUserAltSlash, faEye, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faLaptop, faUser, faBell, faHistory, faBars, faUserPlus, faBookmark, faComments, faArchive, faAddressCard)
+library.add(faLaptop, faUser, faBell, faHistory, faBars, faUserPlus, faBookmark, faComments, faArchive, faAddressCard, faUsers, faUserAltSlash, faEye, faThumbsUp)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
